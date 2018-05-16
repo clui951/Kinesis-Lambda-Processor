a0a6cbaf9af040ee61b933eee3729a0c03946f0e
Calvin Lui <clui@quantcast.com>
2018-05-16 13:16:44 -0700

    add prod/staging db option

--- lambda/rds_config.py
+++ lambda/rds_config.py
@@ -1,7 +1,11 @@
 # config file containing credentials for rds postgres instance
 import os
 
-db_endpoint = os.getenv('db_endpoint') or "<db_endpoint>"
-db_username = os.getenv('db_username') or "<db_username>"
-db_password = os.environ('db_password') or "<db_password>"
-db_name = os.environ('db_name') or "<db_name>" 
+if os.getenv('env') == 'production':
+	db_endpoint = os.getenv('db_endpoint') or ""
+else:
+	db_endpoint = os.getenv('db_endpoint') or ""
+
+db_username = os.getenv('db_username') or ""
+db_password = os.getenv('db_password') or ""
+db_name = os.getenv('db_name') or ""
